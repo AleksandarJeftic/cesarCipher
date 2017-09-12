ff62da3fe744d2f30fd988f2275c042eb3f0b33d
AleksandarJeftic <aleksandarjeftic11@gmail.com>
2017-09-13 01:07:24 +0200

    Added some comments and updated README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# cesarCipher
+Cesar cipher Assignment
+
+Znam da sam trebao prvo testove da pisem pa onda kod, nisam mogao bas da se snadjem pa sam se posluzio obrnutim inzenjeringom.
--- lib/cesar_cipher.rb
+++ lib/cesar_cipher.rb
@@ -1,8 +1,10 @@
 class CesarCipher
+	#class method to avoid unnecessary instantiating
 	def self.rotate (str,num)
-	
+	#arrays for chara scan
 		alphabetLow = ("a".."z").to_a
 		alphabetUp = ("A".."Z").to_a
+	#lowercase and than uppercase character replacing with rotated characters
 		second_step=str.tr(alphabetLow.join,alphabetLow.rotate(num).join)
 		second_step.tr(alphabetUp.join,alphabetUp.rotate(num).join)
 
