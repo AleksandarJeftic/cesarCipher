59e39b92cc2cdd5ccfaa9058e6463fd4a533f0c4
AleksandarJeftic <aleksandarjeftic11@gmail.com>
2017-09-13 00:44:31 +0200

    Written proper tests ,that was impossible before proper refactor

--- spec/cesar_cipher_spec.rb
+++ spec/cesar_cipher_spec.rb
@@ -1,9 +1,37 @@
 require "cesar_cipher"
 
-describe "rotate"  do
-	context "given 'ROT0..26 some string'" do
-		it "returns deciphered message" do
-			expect(rotate("some text",0)).to eql("some text")
+describe CesarCipher  do
+	describe ".rotate" do
+
+
+		context "when number 0 is provided" do
+			let(:num) {0}
+
+			it "returns the same text that is provided" do
+			str="Some text"
+				expect(CesarCipher.rotate(str, num)).to eql("Some text")
+			end 
 		end
+
+		context "when number 26 is provided" do
+			let(:num){26}
+
+			it "returns the same text that is provided" do
+			str="Some text"
+				expect(CesarCipher.rotate(str,num)).to eql("Some text")
+			end
+		end
+
+		context "when number 1 is provided" do 
+			let(:num){1}
+
+			it "encrypts the given string by rotating it a single character" do
+			str="Some text"
+				expect(CesarCipher.rotate(str,num)).to eql("Tpnf ufyu")
+			end
+		end
+
+
 	end
 end
+		
