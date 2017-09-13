ee970108792a36bfd66c188007027871664ebfc0
AleksandarJeftic <aleksandarjeftic11@gmail.com>
2017-09-13 16:52:17 +0200

    deleted extra lines

--- lib/cesar_cipher.rb
+++ lib/cesar_cipher.rb
@@ -1,9 +1,7 @@
 class CesarCipher
   #class method to avoid unnecessary instantiating
-    def self.rotate (str,num)
-      
+    def self.rotate (str,num)  
       str=str.split("")
-
       str=str.map do |char|
       	char=(char.ord+num)
         unless (char>97 && char<122) || (char>65 && char<90)
@@ -11,9 +9,7 @@ class CesarCipher
           char.chr
         else
           char.chr
-            
         end
     return str.join
-    end
-    
+    end  
 end
