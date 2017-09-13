cfb840c9f2294a2a5b4983d3480898d4987472dc
AleksandarJeftic <aleksandarjeftic11@gmail.com>
2017-09-13 16:38:09 +0200

    Third way done

--- lib/cesar_cipher.rb
+++ lib/cesar_cipher.rb
@@ -1,36 +1,18 @@
 class CesarCipher
   #class method to avoid unnecessary instantiating
     def self.rotate (str,num)
-      #arrays for chara scan
-      alphabetLow = ("a".."z").to_a
-      alphabetUp = ("A".."Z").to_a
       
       str=str.split("")
 
-      while num >25
-      	num -= 26
-      end
-
       str=str.map do |char|
-      	if alphabetLow.include?(char)
-      	  n=alphabetLow.index(char) + num
-      	  while n>25
-      	  	n -= 26
-      	  end
-      	  char=alphabetLow[n]
-
-      	elsif alphabetUp.include?(char)
-      	  m=alphabetUp.index(char) + num
-      	  while m>25
-      	  	m -= 26
-      	  end
-      	  char=alphabetUp[m]
-
-      	else
-      	  char
-      	end
-      char
-      end
+      	char=(char.ord+num)
+        unless (char>97 && char<122) || (char>65 && char<90)
+          char-=25
+          char.chr
+        else
+          char.chr
+            
+        end
     return str.join
     end
     
